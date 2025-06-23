93cc63b55f7d77951278bed9a5d6ab094a3b2c4b
ArifAhmadThalha <arif.ahmadthalha@gmail.com>
2025-06-23 11:41:18 +0800

    NEW Structure and Delete Endpoint for progress

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -6,6 +6,6 @@ load_dotenv()
 DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "ayam")
 DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_NAME = os.getenv("DB_NAME", "kKangkung A.I")
+DB_NAME = os.getenv("DB_NAME", "Kangkung A.I")
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
