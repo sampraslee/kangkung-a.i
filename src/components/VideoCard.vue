5c5276334508edf7f0a3bca703febcff1ff6f19e
Brian Choo <bchoo.ws@gmail.com>
2025-07-02 08:09:23 +0800

    Fixed vegetableSelect to vegetableInformation

--- src/components/VideoCard.vue
+++ src/components/VideoCard.vue
@@ -1,62 +1,34 @@
 <template>
-  <v-card class="mb-4">
-    <v-img
-      src="https://placehold.co/600x400"
-      height="200px"
-      cover
-    ></v-img>
-    <v-card-title>How to Grow Kangkung (Water Spinach)</v-card-title>
-    <v-card-text>
-      This video shows step-by-step instructions for growing kangkung at home.
-    </v-card-text>
-    <v-card-actions>
-      <v-btn
-        color="primary"
-        href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-        target="_blank"
-      >
-        Watch Video
-      </v-btn>
-    </v-card-actions>
-  </v-card>
-</template>
-
-<script>
-export default {
-  name: "VideoCard"
-};
-</script>
-
-
-<!-- <template>
-  <v-card class="d-flex flex-column ga-4 pa-4 rounded-lg">
-    <img :src="props.videoThumbnail" class="video-thumbnail">
-    </img>
-    <div class="video-info">
-      <v-card-title class="pa-0">{{ videoTitle }}</v-card-title>
-      <v-card-text class="pa-0">{{ videoSummary }}</v-card-text>
-      <a :href="props.videoLink" class="text-color-primary">Watch Video</a>
-    </div>
-  </v-card>
+  <a :href="'https://www.google.com'" target="_blank" style="text-decoration: none;">
+    <v-card class="d-flex ga-4 pa-4 rounded-lg" style="cursor: pointer;">
+      <img :src="videoThumbnail" class="videoThumbnail" />
+      <div class="video-info">
+        <v-card-title class="pa-0">{{ videoTitle }}</v-card-title>
+        <v-card-text class="pa-0">
+          {{ videoSummary }}
+        </v-card-text>
+      </div>
+    </v-card>
+  </a>
 </template>
 
 <script setup lang="ts">
 const props = defineProps({
   videoThumbnail: {
     type: String,
-    default: "https://placehold.co/40@3x.png"
+    default: "https://placehold.co/40@3x.png",
   },
   videoTitle: {
     type: String,
-    default: "Video title"
+    default: "Video Title XXX",
   },
   videoSummary: {
     type: String,
-    default: "This is an A.I generated summary of the video."
+    default: "This is an A.I generated summary of the video.",
   },
   videoLink: {
     type: String,
     default: "#"
   }
 })
-</script> -->
+</script>
