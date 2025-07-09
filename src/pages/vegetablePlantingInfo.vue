3b59a819cb20dc23f9588a3abcfb5f1d89301e5b
Brian Choo <bchoo.ws@gmail.com>
2025-07-09 14:03:39 +0800

    Update How to grow section for the vegetablePlantingInfo page

--- src/pages/vegetablePlantingInfo.vue
+++ src/pages/vegetablePlantingInfo.vue
@@ -4,71 +4,45 @@
   <!-- Kangkung Info -->
   <div class="vegetable-info bg-accent pa-4 rounded-lg mb-6">
     <h3 class="mb-4">{{ vegetableStore.selectedVegetable.name }}</h3>
-    <v-chip
-      prepend-icon="mdi-clock-time-four-outline"
-      variant="text"
-      class="mb-3"
-    >
+    <v-chip prepend-icon="mdi-clock-time-four-outline" variant="text" class="mb-3">
       Estimated time to harvest:
-      <span
-        :style="{ marginLeft: '4px', fontWeight: 600 }"
-        v-html="
+      <span :style="{ marginLeft: '4px', fontWeight: 600 }" v-html="
           vegetableStore.selectedVegetable.estimated_harvest_time_formatted
-        "
-      ></span>
+        "></span>
     </v-chip>
     <v-chip prepend-icon="mdi-water-outline" variant="text" class="mb-3">
       Watering frequency:
-      <span
-        :style="{ marginLeft: '4px', fontWeight: 600 }"
-        v-html="vegetableStore.selectedVegetable.watering_frequency_formatted"
-      ></span>
+      <span :style="{ marginLeft: '4px', fontWeight: 600 }"
+        v-html="vegetableStore.selectedVegetable.watering_frequency_formatted"></span>
     </v-chip>
     <v-chip prepend-icon="mdi-weather-sunny" variant="text">
       Amount of sunlight:
-      <span
-        :style="{ marginLeft: '4px', fontWeight: 600 }"
-        v-html="vegetableStore.selectedVegetable.amount_of_sunlight"
-      ></span>
+      <span :style="{ marginLeft: '4px', fontWeight: 600 }"
+        v-html="vegetableStore.selectedVegetable.amount_of_sunlight"></span>
     </v-chip>
   </div>
 
   <!-- How to grow -->
   <section id="planting-instructions" class="mb-6">
-    <h3 class="mb-3">How to grow</h3>
-    <v-list class="pa-0">
-      <v-list-item class="pa-0"
-        >1. Choose a container or garden bed with good drainage.</v-list-item
-      >
-      <v-list-item class="pa-0"
-        >2. Fill with nutrient-rich, moist soil or compost.</v-list-item
-      >
-      <v-list-item class="pa-0"
-        >3. Sow seeds or plant cuttings about 1-2 inches deep.</v-list-item
-      >
-      <v-list-item class="pa-0"
-        >4. Keep the soil consistently moist, but not waterlogged.</v-list-item
-      >
-      <v-list-item class="pa-0"
-        >5. Place in an area receiving full sunlight (at least 6 hours
-        daily).</v-list-item
-      >
-      <v-list-item class="pa-0"
-        >6. Sow seeds or plant cuttings about 1-2 inches deep.</v-list-item
-      >
-    </v-list>
+    <h3>How to grow</h3>
+    <v-container class="ps-8">
+      <ol>
+        <li class="mb-2">Choose a container or garden bed with good drainage.</li>
+        <li class="mb-2">Fill with nutrient-rich, moist soil or compost.</li>
+        <li class="mb-2">Sow seeds or plant cuttings about 1-2 inches deep.</li>
+        <li class="mb-2">Keep the soil consistently moist, but not waterlogged.</li>
+        <li class="mb-2">Place in an area receiving full sunlight (at least 6 hours daily).</li>
+        <li>Sow seeds or plant cuttings about 1-2 inches deep.</li>
+      </ol>
+    </v-container>
   </section>
 
   <!-- What you'll need -->
   <section id="materials-needed" class="mb-6">
     <h3 class="mb-4">What you'll need</h3>
-    <MaterialsCard
-      v-for="material in uniqueMaterials"
-      :key="material.id"
-      :materialImageUrl="material.image || 'https://placehold.co/40@3x.png'"
-      :materialName="material.name"
-      :materialDescription="material.description"
-    >
+    <MaterialsCard v-for="material in uniqueMaterials" :key="material.id"
+      :materialImageUrl="material.image || 'https://placehold.co/40@3x.png'" :materialName="material.name"
+      :materialDescription="material.description">
     </MaterialsCard>
   </section>
 
