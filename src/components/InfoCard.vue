0777db9a61d228896972a1c1fc79710435f3678c
Sampras Lee <sampras.lee8@gmail.com>
2025-07-23 16:36:10 +0800

    Created a ToDo and Info card component.

---
+++ src/components/InfoCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card
+    class="pa-6 ga-4 border-sm rounded-lg d-flex flex-row"
+    border="none"
+    elevation="0"
+  >
+    <v-img src="/public/images/info.png" width="180"></v-img>
+    <div class="information">
+      <h3>For your information</h3>
+      <p>
+        Based on the weather forecast for this week, we recommed you to keep
+        kangkung indoors due to the hot weather.
+      </p>
+    </div>
+  </v-card>
+</template>
+<script></script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,25 @@
-<template></template>
+<template>
+  <section id="user-welcome">
+    <h1>Good afternoon, User</h1>
+    <p>Welcome back, let's get you up to speed with your growing vegetables</p>
+  </section>
+  <hr />
+  <section id="dashboard">
+    <v-row>
+      <v-col col="6">
+        <ToDoCard></ToDoCard>
+      </v-col>
+      <v-col col="6">
+        <InfoCard></InfoCard>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col></v-col>
+    </v-row>
+  </section>
+</template>
 
-<script></script>
+<script>
+import ToDoCard from "@/components/ToDoCard.vue";
+import InfoCard from "@/components/InfoCard.vue";
+</script>
