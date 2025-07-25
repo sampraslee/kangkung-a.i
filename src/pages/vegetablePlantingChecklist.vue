2cf3974f72036abefc32ed69ff9e0aef28371a5f
Sampras Lee <sampras.lee8@gmail.com>
2025-07-25 09:53:37 +0800

    Corrected the route to return to dashboard

--- src/pages/vegetablePlantingChecklist.vue
+++ src/pages/vegetablePlantingChecklist.vue
@@ -3,11 +3,17 @@
     <!-- Column 1 -->
     <v-col cols="5" id="selected-vegetable">
       <div class="sticky-card">
-        <VegetableChecklistCard :vegetable-image-url="selectedVegetable?.image_url"
-          :vegetable-name="selectedVegetable?.name" :vegetable-id="selectedVegetable?.id"
-          :estimated-harvest-time="selectedVegetable?.estimated_harvest_time_formatted"
+        <VegetableChecklistCard
+          :vegetable-image-url="selectedVegetable?.image_url"
+          :vegetable-name="selectedVegetable?.name"
+          :vegetable-id="selectedVegetable?.id"
+          :estimated-harvest-time="
+            selectedVegetable?.estimated_harvest_time_formatted
+          "
           :watering-frequency="selectedVegetable?.watering_frequency_formatted"
-          :amount-of-sunlight="selectedVegetable?.amount_of_sunlight" width="360">
+          :amount-of-sunlight="selectedVegetable?.amount_of_sunlight"
+          width="360"
+        >
         </VegetableChecklistCard>
       </div>
     </v-col>
@@ -16,18 +22,20 @@
     <!-- Instructions -->
     <v-col cols="7" id="planting-guide">
       <div class="instructions mb-4">
-        <h1 class="mb-4">Ready to start? </h1>
-        <p>
-          Grab these items, then tap 'Proceed'!
-        </p>
+        <h1 class="mb-4">Ready to start?</h1>
+        <p>Grab these items, then tap 'Proceed'!</p>
       </div>
 
       <!-- Materials -->
       <div class="what-you-need">
         <h2 class="mb-4">What you'll need:</h2>
-        <ChecklistItemCard v-for=" material in uniqueMaterials" :key="material.id"
-          :materialImageUrl="material.image || 'https://placehold.co/40@3x.png'" :materialName="material.name"
-          :materialDescription="material.description">
+        <ChecklistItemCard
+          v-for="material in uniqueMaterials"
+          :key="material.id"
+          :materialImageUrl="material.image || 'https://placehold.co/40@3x.png'"
+          :materialName="material.name"
+          :materialDescription="material.description"
+        >
         </ChecklistItemCard>
       </div>
 
@@ -42,15 +50,14 @@
 
       <!-- Calendar -->
       <div>
-        <h2 class="mb-4"> When would you like to start planting?</h2>
+        <h2 class="mb-4">When would you like to start planting?</h2>
         <Calendar class="mb-5"></Calendar>
       </div>
 
       <!-- Call to Action Button -->
-      <CallToActionButton :to="`/dashboard`" class="mb-4">
+      <CallToActionButton :to="`/`" class="mb-4">
         I'm ready to plant
       </CallToActionButton>
-
     </v-col>
   </v-row>
 </template>
